76a2d594360b9ac180ab1fc6926d2f2476caefbc
sshatil <mutasimshatil@hotmail.com>
2025-01-06 12:25:19 +0800

    feat: setup nvim config

---
+++ lua/shatil/core/options.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- Enable absolute line number for the current line
