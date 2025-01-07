71607394d635c6fc050fcafca5831277795fcf02
sshatil <mutasimshatil@hotmail.com>
2025-01-08 01:23:27 +0800

    feat: setup formatting & linting

--- lua/shatil/plugins/lsp/mason.lua
+++ lua/shatil/plugins/lsp/mason.lua
@@ -1,39 +1,50 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "graphql",
-        "prismals",
-        "emmet_ls",
-      },
-      -- automatically install configured servers
-      automatic_installation = true, -- or false if you don't want this
-    })
-  end,
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"graphql",
+				"prismals",
+				"emmet_ls",
+			},
+			-- automatically install configured servers
+			automatic_installation = true, -- or false if you don't want this
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"eslint_d", -- js linter
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+			},
+		})
+	end,
 }
